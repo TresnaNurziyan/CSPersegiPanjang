6d0009cc681430bae06403db8059845e383e6bda
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2017-07-26 11:28:31 +0700

    Praktikum C# menghitung persegi panjang

---
+++ CSPanjangLebar/CSPP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPanjangLebar
+{
+    class CSPP
+    {
+        static void Main(string[] args)
+        {
+            int panjang;
+            int lebar;
+
+            Console.Write("Masukan panjang  : ");
+            panjang = int.Parse(Console.ReadLine());
+            Console.Write("Masukan lebar    : ");
+            lebar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Luas persegi panjang     : " + panjang * lebar);
+            Console.WriteLine("Keliling persegi panjang : " + 2 * (panjang + lebar));
+            Console.WriteLine("----------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
